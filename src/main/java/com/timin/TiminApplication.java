83707f9ccd2b35c9fccb4399f74615557240aef6
西田直矢 <naoya@nishidanaoyanoMacBook-Pro.local>
2017-11-18 12:39:48 +0900

    first commit

---
+++ src/main/java/com/timin/TiminApplication.java
@@ -0,0 +1,12 @@
+package com.timin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiminApplication.class, args);
+	}
+}
---
+++ src/main/java/com/timin/controller/IndexControlloer.java
@@ -0,0 +1,18 @@
+package com.timin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by naoya on 2017/10/29.
+ */
+@Controller("/")
+public class IndexControlloer {
+
+    @RequestMapping("/")
+    public String index(ModelAndView modelAndView) {
+        return "index";
+    }
+
+}
