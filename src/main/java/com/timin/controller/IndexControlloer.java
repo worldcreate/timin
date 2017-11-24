cb9a0abace8bdbf668d109c852c3cefc1a6f8504
worldcreate <sogrnwil2907@gmail.com>
2017-11-24 23:37:08 +0900

    domaの設定を追加

--- src/main/java/com/timin/controller/IndexControlloer.java
+++ src/main/java/com/timin/controller/IndexControlloer.java
@@ -1,8 +1,11 @@
 package com.timin.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.timin.service.SampleService;
 
 /**
  * Created by naoya on 2017/10/29.
@@ -10,8 +13,13 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller("/")
 public class IndexControlloer {
 
+    @Autowired
+    SampleService sampleService;
+
     @RequestMapping("/")
     public String index(ModelAndView modelAndView) {
+        System.out.println(sampleService.findAll());
+
         return "index";
     }
 
