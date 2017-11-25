63b6324af52f4a97e6b4457173e2b2f12f2181f1
worldcreate <sogrnwil2907@gmail.com>
2017-11-25 23:59:20 +0900

    ログの設定の追加

--- src/main/java/com/timin/controller/IndexControlloer.java
+++ src/main/java/com/timin/controller/IndexControlloer.java
@@ -1,10 +1,15 @@
 package com.timin.controller;
 
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.timin.entity.Employee;
 import com.timin.service.SampleService;
 
 /**
@@ -13,12 +18,17 @@ import com.timin.service.SampleService;
 @Controller("/")
 public class IndexControlloer {
 
+    private static final Logger logger = LogManager.getLogger(IndexControlloer.class);
+    
     @Autowired
     SampleService sampleService;
 
     @RequestMapping("/")
     public String index(ModelAndView modelAndView) {
-        System.out.println(sampleService.findAll());
+        List<Employee> employees = sampleService.findAll();
+        employees.forEach(entity -> {
+            logger.info("id={}, name={}, age={}", entity.getId(), entity.getName(), entity.getAge());
+        });
 
         return "index";
     }
